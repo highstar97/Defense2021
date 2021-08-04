a574e7a147dfd6e900b7031bc6a8b83c8c0a5603
chati97 <67995114+chati97@users.noreply.github.com>
2021-08-04 22:50:54 +0900

    modify(bug O)

---
+++ Defense2021/Assets/Scripts/Ingame/Testing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject ob1;
+    public void TestClick()
+    {
+        Destroy(ob1, 0);
+    }
+}
