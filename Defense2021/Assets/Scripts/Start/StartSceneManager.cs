a837a15add72ce4c3ca86894f62e1fe8712c2f38
highstar97 <pgt0185@gmail.com>
2021-08-09 21:52:08 +0900

    StartSceneManager 생성
    기존 StartScene의 DirectorManager && SettingManger를 StartSceneManager로 통합

---
+++ Defense2021/Assets/Scripts/Start/StartSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneManager : MonoBehaviour
+{
+    public GameObject SettingMenu;
+    public GameObject DirectorMenu;
+    void Update(){
+        if(DirectorMenu.activeSelf == true && Input.GetKeyDown(KeyCode.Escape)){
+            DirectorMenu.SetActive(false);
+        }
+        else if(DirectorMenu.activeSelf == false && Input.GetKeyDown(KeyCode.Escape)){
+            SettingMenu.SetActive(false);
+        }
+    }
+    public void OpenSetting(){
+        SettingMenu.SetActive(true);
+    }
+    public void CloseSetting(){
+        SettingMenu.SetActive(false);
+    }
+    public void OpenDirector(){
+        DirectorMenu.SetActive(true);
+    }
+    public void CloseDirector(){
+        DirectorMenu.SetActive(false);
+    }
+}
