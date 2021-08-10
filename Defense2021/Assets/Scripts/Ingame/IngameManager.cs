7c15120db51876c59e76cf1c25253ee8321f3aa9
highstar97 <pgt0185@gmail.com>
2021-08-10 19:46:52 +0900

    Ingame UI 기본 구현
    Ingame UI 기본 기능 구현

---
+++ Defense2021/Assets/Scripts/Ingame/IngameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngameManager : MonoBehaviour
+{
+    public GameObject DictionaryMenu;
+    public GameObject SettingMenu;
+    public GameObject DirectorMenu;
+    public GameObject LvupMenu;
+    public GameObject BuffStatus;
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape) && DirectorMenu.activeSelf == true){
+            DirectorMenu.SetActive(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && SettingMenu.activeSelf == true){
+            SettingMenu.SetActive(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && DictionaryMenu.activeSelf == true){
+            DictionaryMenu.SetActive(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && LvupMenu.activeSelf == true){
+            LvupMenu.SetActive(false);
+        }
+    }
+    public void OpenDictionary(){
+        DictionaryMenu.SetActive(true);
+    }
+    public void CloseDictionary(){
+        DictionaryMenu.SetActive(false);
+    }
+    public void OpenSetting(){
+        SettingMenu.SetActive(true);
+    }
+    public void CloseSetting(){
+        SettingMenu.SetActive(false);
+    }
+    public void OpenDirector(){
+        DirectorMenu.SetActive(true);
+    }
+    public void CloseDirector(){
+        DirectorMenu.SetActive(false);
+    }
+    public void OpenLvup(){
+        LvupMenu.SetActive(true);
+    }
+    public void CloseLvup(){
+        LvupMenu.SetActive(false);
+    }
+    public void PushBuffButton(){
+        if(BuffStatus.activeSelf == false){
+            BuffStatus.SetActive(true);
+        }
+        else if(BuffStatus.activeSelf == true){
+            BuffStatus.SetActive(false);
+        }
+    }
+}
--- Defense2021/Assets/Scripts/Start/StartSceneManager.cs
+++ Defense2021/Assets/Scripts/Start/StartSceneManager.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartSceneManager : MonoBehaviour
 {
     public GameObject SettingMenu;
     public GameObject DirectorMenu;
     void Update(){
-        if(DirectorMenu.activeSelf == true && Input.GetKeyDown(KeyCode.Escape)){
+        if(Input.GetKeyDown(KeyCode.Escape) && DirectorMenu.activeSelf == true){
             DirectorMenu.SetActive(false);
         }
-        else if(DirectorMenu.activeSelf == false && Input.GetKeyDown(KeyCode.Escape)){
+        else if(Input.GetKeyDown(KeyCode.Escape) && SettingMenu.activeSelf == true){
             SettingMenu.SetActive(false);
         }
     }
