57123dc3eeca8f980a70ecfd677b34720273f1a0
troyiz <81087331+troyiz@users.noreply.github.com>
2021-08-29 17:09:53 +0900

    .

---
+++ Defense2021/Assets/Scripts/Ingame/Damage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Damage : MonoBehaviour
+{
+    public GameObject DamageText;
+    public GameObject TextPos;
+
+    public void GetDamage(int damage)
+    {
+        GameObject dmgText = Instantiate(DamageText, TextPos.transform.position, Quaternion.identity);
+        dmgText.GetComponent<Text>().text = damage.ToString();
+    }
+}
