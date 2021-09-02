104eec93534e250962e51cda0bc26939df2520bc
jihwanjeong <53459838+jihwanjeong@users.noreply.github.com>
2021-09-02 21:58:53 +0900

    라운드 임시구현

--- Defense2021/Assets/Scripts/Ingame/BuffManager.cs
+++ Defense2021/Assets/Scripts/Ingame/BuffManager.cs
@@ -7,18 +7,28 @@ public class BuffManager : MonoBehaviour
     public bool IsActive = false;
     public Button KingSkillButton;
     public Text Bufftext;
+    GoldManager goldmanager;
 
     public void HumanKingSkill()
     {
         Bufftext.text = "HumanKing's Buff(Gold x5)";
         KingSkillButton.transform.GetChild(0).GetComponent<Text>().text = "Active";
         IsActive = true;
+        goldmanager.GoldColor += 1;
         Invoke("DisableHumanKingSkill",5f);
     }
 
     public void DisableHumanKingSkill(){
+        goldmanager.GoldColor -= 1;
         IsActive = false;
         Bufftext.text = "";
         KingSkillButton.transform.GetChild(0).GetComponent<Text>().text = "King Skill";
+    }
+
+    void Start()
+    {
+        goldmanager = GameObject.Find("Gold Manager").GetComponent<GoldManager>();
+
+
     }
 }
