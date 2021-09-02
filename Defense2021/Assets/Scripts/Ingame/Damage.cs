57be1075ddee1d895ec13198fd737d767313dc25
troyiz <81087331+troyiz@users.noreply.github.com>
2021-09-02 14:29:44 +0900

    damage & hpbar

--- Defense2021/Assets/Scripts/Ingame/Damage.cs
+++ Defense2021/Assets/Scripts/Ingame/Damage.cs
@@ -8,9 +8,18 @@ public class Damage : MonoBehaviour
     public GameObject DamageText;
     public GameObject TextPos;
 
+    public float StartHealth;
+    public float Health;
+
+    public GameObject HealthBar;
+
     public void GetDamage(int damage)
     {
         GameObject dmgText = Instantiate(DamageText, TextPos.transform.position, Quaternion.identity);
         dmgText.GetComponent<Text>().text = damage.ToString();
+        Health -= damage;
+        HealthBar.GetComponent<Image>().fillAmount = Health / StartHealth;
+
+        Destroy(dmgText, 0.5f); 
     }
 }
