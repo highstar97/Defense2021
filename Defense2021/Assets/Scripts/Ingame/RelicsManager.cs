1a4d8e70a583dd630c8c05e70deef8086c3b720b
jihwanjeong <53459838+jihwanjeong@users.noreply.github.com>
2021-09-08 17:50:20 +0900

    1

--- Defense2021/Assets/Scripts/Ingame/RelicsManager.cs
+++ Defense2021/Assets/Scripts/Ingame/RelicsManager.cs
@@ -7,8 +7,13 @@ public class RelicsManager : MonoBehaviour
 {
 
     public GameObject GoldRelic;
+    public GameObject SwordRelic;
+    public GameObject AppleRelic;
+    Stats stats;
     GoldManager goldmanager;
     public bool Goldset = false;
+    public bool Swordset = false;
+    public bool Appleset = false;
 
 
     public void GoldRelicEnable()
@@ -23,15 +28,41 @@ public class RelicsManager : MonoBehaviour
             goldmanager.GoldColor -= 1;
             Goldset = false;
         }
+    }
+    public void AppleRelicEnable()
+    {
+        if (AppleRelic.activeSelf == true && Appleset == false)
+        {
 
+            
+        }
+        else if (AppleRelic.activeSelf == false && Appleset == true)
+        {
+           
+        }
+    }
 
+    public void SwordRelicEnable() 
+    {
+        if (SwordRelic.activeSelf == true && Swordset ==false)
+        {
+            stats.ATK *= 2;
+            Swordset = true;
+        }
+        else if (SwordRelic.activeSelf == false && Swordset == true)
+        {
+            stats.ATK *=0.5f;
+            Swordset = false;
+        }
 
     }
+
 
     // Start is called before the first frame update
     void Start()
     {
         goldmanager = GameObject.Find("Gold Manager").GetComponent<GoldManager>();
+        stats = GameObject.Find("Enemy").GetComponent<Stats>();
 
 
     }
@@ -40,5 +71,6 @@ public class RelicsManager : MonoBehaviour
     void Update()
     {
         GoldRelicEnable();
+        SwordRelicEnable();
     }
 }
