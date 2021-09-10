eabb27f9089fbec201eb7e598e15938625d5bd65
highstar97 <pgt0185@gmail.com>
2021-09-10 22:47:40 +0900

    Boss Panel 생성

---
+++ Defense2021/Assets/Scripts/Ingame/EventSituation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSituation
+{
+    bool IsUsed = false;
+    int NumofOptions;
+    string Situation;
+    string[] option;
+}
