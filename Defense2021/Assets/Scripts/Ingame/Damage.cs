944ac211890503aa8f8351b3377093a55d884928
troyiz <81087331+troyiz@users.noreply.github.com>
2021-09-11 00:57:57 +0900

    why?

--- Defense2021/Assets/Scripts/Ingame/Damage.cs
+++ Defense2021/Assets/Scripts/Ingame/Damage.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class Damage : MonoBehaviour
 {
-    public GameObject DamageText;
-    public GameObject TextPos;
 
     public float StartHealth;
     public float Health;
@@ -15,11 +13,8 @@ public class Damage : MonoBehaviour
 
     public void GetDamage(int damage)
     {
-        GameObject dmgText = Instantiate(DamageText, TextPos.transform.position, Quaternion.identity);
-        dmgText.GetComponent<Text>().text = damage.ToString();
         Health -= damage;
         HealthBar.GetComponent<Image>().fillAmount = Health / StartHealth;
 
-        Destroy(dmgText, 0.5f); 
     }
 }
