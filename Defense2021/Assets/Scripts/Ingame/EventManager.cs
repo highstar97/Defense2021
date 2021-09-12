db0f54d383d2bbb7261dae55714ba673bd7d95d7
highstar97 <pgt0185@gmail.com>
2021-09-12 22:29:51 +0900

    Event 출력 함수 생성

--- Defense2021/Assets/Scripts/Ingame/EventManager.cs
+++ Defense2021/Assets/Scripts/Ingame/EventManager.cs
@@ -1,34 +1,54 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EventManager : MonoBehaviour
 {
-    float distance;
-    Vector3 Base;
-    public GameObject Choice1;
-    public GameObject Choice2;
-    public GameObject Choice3;
-    public GameObject Select;
-    public GameObject ChoiceActiveAnimation;
-
-    public void ClickChoice1(){
-        ChoiceActiveAnimation.transform.position = new Vector3(Base.x,Base.y,Base.z);
-        ChoiceActiveAnimation.SetActive(true);
-    }
-    public void ClickChoice2(){
-        ChoiceActiveAnimation.transform.position = new Vector3(Base.x,Base.y+distance,Base.z);
-        ChoiceActiveAnimation.SetActive(true);
-    }
-    public void ClickChoice3(){
-        ChoiceActiveAnimation.transform.position = new Vector3(Base.x,Base.y+2*distance,Base.z);
-        ChoiceActiveAnimation.SetActive(true);
-    }
-    public void ClickSelect(){
-        ChoiceActiveAnimation.SetActive(false);
-    }
+    [SerializeField] Text Situation;
+    [SerializeField] GameObject Choice1;
+    [SerializeField] GameObject Choice2;
+    [SerializeField] GameObject Choice3;
+    EventSituation[] EventSituations;
     void Start(){
-        distance = Choice2.transform.position.y - Choice1.transform.position.y;
-        Base = ChoiceActiveAnimation.transform.position;
+        EventSituations = new EventSituation[3];
+        for(int i=0; i<3; i++){
+            EventSituations[i] = new EventSituation();
+        }
+        string[] options = new string[3] {"불의 정령에게 준다", "물의 정령에게 준다", "땅의 정령에게 준다"};
+        EventSituations[0].MakeEvent("피닉스의 날개를 발견했습니다.", 3, options);
+        options = new string[3] {"이딴게 신탁?", "알잘딱하자~", "응 아니야~"};
+        EventSituations[1].MakeEvent("신탁을 들었습니다", 3, options);
+        options = new string[1] {"OK"};
+        EventSituations[2].MakeEvent("돈을 주웠습니다. 가지겠습니까?", 1, options);
+    }
+    public void EventOccur(){
+        int x;
+        do{
+            x = Random.Range(0,3); // [0,3)
+        }while(EventSituations[x].GetIsUsed());
+        EventSituations[x].SetIsUsedTrue();
+        Situation.text = EventSituations[x].GetSituation();
+        if(EventSituations[x].GetNumofOptions() == 3){
+            Choice1.SetActive(true);
+            Choice2.SetActive(true);
+            Choice3.SetActive(true);
+            Choice1.transform.GetChild(0).GetComponent<Text>().text = EventSituations[x].GetOption()[0];
+            Choice2.transform.GetChild(0).GetComponent<Text>().text = EventSituations[x].GetOption()[1];
+            Choice3.transform.GetChild(0).GetComponent<Text>().text = EventSituations[x].GetOption()[2];
+        }
+        else if(EventSituations[x].GetNumofOptions() == 2){
+            Choice1.SetActive(true);
+            Choice2.SetActive(true);
+            Choice3.SetActive(false);
+            Choice1.transform.GetChild(0).GetComponent<Text>().text = EventSituations[x].GetOption()[0];
+            Choice2.transform.GetChild(0).GetComponent<Text>().text = EventSituations[x].GetOption()[1];
+        }
+        else if(EventSituations[x].GetNumofOptions() == 1){
+            Choice1.SetActive(false);
+            Choice2.SetActive(true);
+            Choice3.SetActive(false);
+            Choice2.transform.GetChild(0).GetComponent<Text>().text = EventSituations[x].GetOption()[0];
+        }
     }
 }
--- Defense2021/Assets/Scripts/Ingame/EventSituation.cs
+++ Defense2021/Assets/Scripts/Ingame/EventSituation.cs
@@ -8,4 +8,33 @@ public class EventSituation
     int NumofOptions;
     string Situation;
     string[] option;
+    public EventSituation(){
+        this.IsUsed = false;
+        this.Situation = "제발";
+        this.NumofOptions = 3;
+        string[] a = {"1","2","3"};
+        this.option = a;
+    }
+    public EventSituation MakeEvent(string Situation, int NumofOptions, string[] option){
+        this.IsUsed = false;
+        this.Situation = Situation;
+        this.NumofOptions = NumofOptions;
+        this.option = option;
+        return this;
+    }
+    public bool GetIsUsed(){
+        return this.IsUsed;
+    }
+    public int GetNumofOptions(){
+        return this.NumofOptions;
+    }
+    public string GetSituation(){
+        return this.Situation;
+    }
+    public string[] GetOption(){
+        return this.option;
+    }
+    public void SetIsUsedTrue(){
+        this.IsUsed = true;
+    }
 }
