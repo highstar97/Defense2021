d66b4e684d3411d86b49ed6d48dbcb3a8a3b5be1
troyiz <81087331+troyiz@users.noreply.github.com>
2021-09-14 10:12:27 +0900

    asdd

---
+++ Defense2021/Assets/Scripts/Damage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Damage : MonoBehaviour
+{
+
+    public float StartHealth;
+    public float Health;
+
+    public GameObject HealthBar;
+
+    public void GetDamage(int damage)
+    {
+        Health -= damage;
+        HealthBar.GetComponent<Image>().fillAmount = Health / StartHealth;
+
+    }
+}
