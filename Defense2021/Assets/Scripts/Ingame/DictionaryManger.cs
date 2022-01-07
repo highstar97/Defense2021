a1147df0915c8538f3636d2557f6667bc3a92f5e
jihwanjeong <53459838+jihwanjeong@users.noreply.github.com>
2022-01-07 22:00:29 +0900

    1

--- Defense2021/Assets/Scripts/Ingame/DictionaryManger.cs
+++ Defense2021/Assets/Scripts/Ingame/DictionaryManger.cs
@@ -9,31 +9,70 @@ public class DictionaryManger : MonoBehaviour
     public GameObject Dictionary;
     public GameObject Tag_King;
     public GameObject Tag_OurUnit;
+    public GameObject Tag_Devil;
+    public GameObject Tag_EnemyUnit;
+    public GameObject Tag_Relics;
     public GameObject BackGround_Dictioanry;
     public GameObject Dictionary_King;
     public GameObject Dictionary_OurUnit;
+    public GameObject Dictionary_Devil;
+    public GameObject Dictionary_EnemyUnit;
+    public GameObject Dictionary_Relics;
 
-    public void TurnOnKing(){
+    public void TurnOnKing()
+    {
         TurnOff(IndexOn);
 
-        Tag_King.GetComponent<Image>().color = new Color(255/255.0f,200/255.0f,0);
-        Tag_King.transform.GetChild(0).GetComponent<Text>().color = new Color(255/255.0f,0,0);
+        Tag_King.GetComponent<Image>().color = new Color(255 / 255.0f, 200 / 255.0f, 0);
+        Tag_King.transform.GetChild(0).GetComponent<Text>().color = new Color(255 / 255.0f, 0, 0);
         Dictionary_King.SetActive(true);
         IndexOn = 0;
     }
 
-    public void TurnOnOurUnit(){
+    public void TurnOnOurUnit()
+    {
         TurnOff(IndexOn);
 
-        Tag_OurUnit.GetComponent<Image>().color = new Color(255/255.0f,200/255.0f,0);
-        Tag_OurUnit.transform.GetChild(0).GetComponent<Text>().color = new Color(255/255.0f,0,0);
+        Tag_OurUnit.GetComponent<Image>().color = new Color(255 / 255.0f, 200 / 255.0f, 0);
+        Tag_OurUnit.transform.GetChild(0).GetComponent<Text>().color = new Color(255 / 255.0f, 0, 0);
         Dictionary_OurUnit.SetActive(true);
         IndexOn = 1;
     }
-    
-    public void TurnOff(int IndexOn){
-        Dictionary.transform.GetChild(IndexOn+3).GetComponent<Image>().color = new Color(110/255.0f,135/255.0f,255/255.0f);
-        Dictionary.transform.GetChild(IndexOn+3).transform.GetChild(0).GetComponent<Text>().color = new Color(0,0,0);
+
+    public void TurnOnDevil()
+    {
+        TurnOff(IndexOn);
+
+        Tag_Devil.GetComponent<Image>().color = new Color(255 / 255.0f, 200 / 255.0f, 0);
+        Tag_Devil.transform.GetChild(0).GetComponent<Text>().color = new Color(255 / 255.0f, 0, 0);
+        Dictionary_Devil.SetActive(true);
+        IndexOn = 2;
+    }
+
+    public void TurnOnEnemyUnit()
+    {
+        TurnOff(IndexOn);
+
+        Tag_EnemyUnit.GetComponent<Image>().color = new Color(255 / 255.0f, 200 / 255.0f, 0);
+        Tag_EnemyUnit.transform.GetChild(0).GetComponent<Text>().color = new Color(255 / 255.0f, 0, 0);
+        Dictionary_EnemyUnit.SetActive(true);
+        IndexOn = 3;
+    }
+
+    public void TurnOnRelics()
+    {
+        TurnOff(IndexOn);
+
+        Tag_Relics.GetComponent<Image>().color = new Color(255 / 255.0f, 200 / 255.0f, 0);
+        Tag_Relics.transform.GetChild(0).GetComponent<Text>().color = new Color(255 / 255.0f, 0, 0);
+        Dictionary_Relics.SetActive(true);
+        IndexOn = 4;
+    }
+
+    public void TurnOff(int IndexOn)
+    {
+        Dictionary.transform.GetChild(IndexOn + 3).GetComponent<Image>().color = new Color(110 / 255.0f, 135 / 255.0f, 255 / 255.0f);
+        Dictionary.transform.GetChild(IndexOn + 3).transform.GetChild(0).GetComponent<Text>().color = new Color(0, 0, 0);
         BackGround_Dictioanry.transform.GetChild(IndexOn).gameObject.SetActive(false);
     }
 }
---
+++ Defense2021/Assets/Scripts/Ingame/EventClickManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventClickManager : MonoBehaviour
+{
+    float distance;
+    Vector3 Base;
+    public GameObject Choice1;
+    public GameObject Choice2;
+    public GameObject Choice3;
+    public GameObject Select;
+    public GameObject ChoiceActiveAnimation;
+
+    public void ClickChoice1()
+    {
+        ChoiceActiveAnimation.transform.position = new Vector3(Base.x, Base.y, Base.z);
+        ChoiceActiveAnimation.SetActive(true);
+    }
+    public void ClickChoice2()
+    {
+        ChoiceActiveAnimation.transform.position = new Vector3(Base.x, Base.y + distance, Base.z);
+        ChoiceActiveAnimation.SetActive(true);
+    }
+    public void ClickChoice3()
+    {
+        ChoiceActiveAnimation.transform.position = new Vector3(Base.x, Base.y + 2 * distance, Base.z);
+        ChoiceActiveAnimation.SetActive(true);
+    }
+    public void ClickSelect()
+    {
+        ChoiceActiveAnimation.SetActive(false);
+    }
+    void Start()
+    {
+        distance = Choice2.transform.position.y - Choice1.transform.position.y;
+        Base = ChoiceActiveAnimation.transform.position;
+    }
+}
